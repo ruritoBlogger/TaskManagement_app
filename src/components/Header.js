66a4af8f3802c0b9e6b9af8b7d0d19327cb2cf99
rurito <rurito.blog@gmail.com>
2020-07-12 00:23:20 +0900

    Header部分を分離した

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1> This is header!</h1>
+      </header>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/Header.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <h2> Hellow World!</h2>
       </div>
     );
   }
 }
+
+const app = document.getElementById('app');
+ReactDOM.render(<App/>, app);
