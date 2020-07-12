f693c9e2e9f3f54751bba45d15449900583cbb35
rurito <rurito.blog@gmail.com>
2020-07-12 17:01:58 +0900

    紹介文を実装

--- src/index.js
+++ src/index.js
@@ -6,12 +6,22 @@ import Footer from './components/Footer.js';
 
 import { makeStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 
 const styles = makeStyles({
   top: {
     backgroundColor: '#33DFEA',
     margin: '0px',
     border: 'opx',
+  },
+  container: {
+    'max-width': '740px',
+    color: '#FFFFFF',
+  },
+  topText: {
+    'text-align': 'center',
+    margin: '5px 0px',
+    'font-weight': '300',
   },
   bottom: {
     backgroundColor: '#FFFFFF',
@@ -24,6 +34,13 @@ export default function App() {
     <Box>
       <Box className={classes.top}>
         <Header />
+        <Container className={classes.container}>
+          <h1 className={classes.topText}>
+            大学の課題と<br />
+            楽しく付き合おう<br />
+            そんな手助けをするアプリ
+          </h1>
+        </Container>
       </Box>
       <Box className={classes.bottom}>
         <Footer />
