a7bba986f839655fdf11565acef59815a10a154e
rurito <rurito.blog@gmail.com>
2020-07-12 18:19:21 +0900

    力技で画像を表示した

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,30 @@ const styles = makeStyles({
     'text-align': 'center',
     margin: '30px 0px',
     'font-weight': '300',
+  },
+  box: {
+    margin: '0px 0px 0px 0px',
+  },
+  pictureBox: {
+    display: 'block',
+    margin: 'auto',
+  },
+  twitterPicture: {
+    display: 'block',
+    margin: 'auto',
+    'text-align': 'center',
+    width: '75%',
+    height: 'auto',
+  },
+  googlePicture: {
+    display: 'block',
+    margin: 'auto',
+    'text-align': 'center',
+    width: '60%',
+    height: 'auto',
+    padding: '27px',
   }
+
 });
 
 export default function Footer() {
@@ -27,6 +50,20 @@ export default function Footer() {
             大学の課題を消化することによる<br />
             メリットを提供します
           </h2>
+        </Grid>
+        <Grid container direction="row">
+          <Grid item xs={6} direction="column" className={classes.box}>
+            <Grid item className={classes.pictureBox}>
+              <img className={classes.twitterPicture} src={`${window.location.origin}/pictures/Twitter_Logo_Blue.png`} />
+            </Grid>
+            <p>Tiwtter</p>
+          </Grid>
+          <Grid item xs={6} direction="column" className={classes.box}>
+            <Grid item className={classes.pictureBox}>
+              <img className={classes.googlePicture} src={`${window.location.origin}/pictures/calender.png`} />
+            </Grid>
+            <p>Google</p>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
