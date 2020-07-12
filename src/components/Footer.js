f81a3daf088c01730f404d83facc4fb0d9c3b926
rurito <rurito.blog@gmail.com>
2020-07-12 17:35:05 +0900

    Footerのメリット部分を実装

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <h1> This is footer!</h1>
-      </footer>
-    )
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
+const styles = makeStyles({
+  container: {
+    'max-width': '740px',
+    color: '#484848',
+  },
+  bottomText: {
+    'text-align': 'center',
+    margin: '30px 0px',
+    'font-weight': '300',
   }
+});
+
+export default function Footer() {
+  const classes = styles();
+  return (
+    <Container className={classes.container}>
+      <Grid container direction="column">
+        <Grid item>
+          <h2 className={classes.bottomText}>
+            大学の課題を消化することによる<br />
+            メリットを提供します
+          </h2>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
