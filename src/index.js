98e814d92a42609f9bba6eb56a2e50c2f1361906
rurito <rurito.blog@gmail.com>
2020-07-13 13:24:30 +0900

    未定義ページを切り離した

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Undefined from './undefined';
 import Introduction from './components/introduction/index';
 import Main from './components/main/index';
-
-export default function Undefined() {
-  return (
-    <div>
-      <h1>未定義ページです</h1>
-    </div>
-  );
-}
 
 const app = document.getElementById('app');
 ReactDOM.render(
---
+++ src/undefined.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Undefined() {
+  return (
+    <div>
+      <h1>未定義ページです</h1>
+    </div>
+  );
+}
