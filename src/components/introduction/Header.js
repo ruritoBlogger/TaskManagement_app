7c37c17a1b9be2ff7334810a6ecacf26198a64bd
rurito <rurito.blog@gmail.com>
2020-07-13 12:04:48 +0900

    紹介画面を表示するjsのディレクトリを変更した

--- src/components/introduction/Header.js
+++ src/components/introduction/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import firebase from '../firebase';
+import firebase from '../../firebase';
 
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
---
+++ src/components/introduction/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+
+const styles = makeStyles({
+  top: {
+    backgroundColor: '#33DFEA',
+    margin: '0px',
+    border: 'opx',
+  },
+  container: {
+    'max-width': '740px',
+    color: '#FFFFFF',
+  },
+  topText: {
+    'text-align': 'center',
+    margin: '30px 0px',
+    'font-weight': '300',
+  },
+  button: {
+    left: '38%',
+    'padding' : '5px 10px',
+    'margin' : '30px',
+    color: '#FFFFFF',
+    background: '#EDC124',
+    'text-align': 'center',
+  },
+  bottom: {
+    backgroundColor: '#FFFFFF',
+  }
+});
+
+export default function Introduction() {
+  const classes = styles();
+  return (
+    <Box>
+      <Box className={classes.top}>
+        <Header />
+        <Container className={classes.container}>
+          <Grid container direction="column">
+            <Grid item>
+              <h1 className={classes.topText}>
+                大学の課題と<br />
+                楽しく付き合おう<br />
+                そんな手助けをするアプリ
+              </h1>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" className={classes.button}>始めてみる</Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      <Box className={classes.bottom}>
+        <Footer />
+      </Box>
+    </Box>
+  );
+}
