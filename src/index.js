19177424e4cd9722ccd5981a4e0f17753e4bfbc6
rurito <rurito.blog@gmail.com>
2020-07-14 17:37:48 +0900

    時間割ページのルーティングを設定

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Undefined from './undefined';
 import Introduction from './components/introduction/index';
 import Main from './components/main/index';
+import Schedule from './components/schedule/index';
 
 const app = document.getElementById('app');
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route exact path="/" component={Introduction}></Route>
       <Route exact path="/main/" component={Main}></Route>
+      <Route exact path="/schedule/" component={Schedule}></Route>
       <Route component={Undefined}></Route>
     </Switch>
   </Router>,
