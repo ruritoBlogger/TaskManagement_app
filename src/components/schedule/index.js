fe03d92ed29038f833a06378e0e7d517ee736ace
rurito <rurito.blog@gmail.com>
2020-07-15 00:02:42 +0900

    時間割画面からユーザー情報にアクセス出来るようにした

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -4,11 +4,11 @@ import { useHistory } from 'react-router-dom';
 import firebase from '../../firebase';
 
 import DefaultDialog from '../dialog';
+import DeleteDialog from '../deleteDialog';
 
 import Button from '@material-ui/core/Button';
-import { DialogTitle } from '@material-ui/core';
 
-export default function Schedule() {
+export default function Schedule(props) {
   const history = useHistory();
 
   function MoveMain() {
@@ -20,6 +20,7 @@ export default function Schedule() {
       <h1>ここは時間割ページ</h1>
       <Button variant="contained" onClick={MoveMain}>ホームページ</Button>
       <DefaultDialog></DefaultDialog>
+      <DeleteDialog Button="削除" msg="うんち"></DeleteDialog>
     </div>
   );
 }
