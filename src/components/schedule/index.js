a5a8fe61fb1253d4d6ee7c3b8f20e5be3a75f0dc
rurito <rurito.blog@gmail.com>
2020-07-14 17:38:03 +0900

    時間割ページを生成

---
+++ src/components/schedule/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import firebase from '../../firebase';
+
+import Button from '@material-ui/core/Button';
+
+
+
+export default function Schedule() {
+
+  return (
+    <div>
+      <h1>ここは時間割ページ</h1>
+    </div>
+  );
+}
