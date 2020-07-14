65ce981324d0eabeacabed6f755ba4a9a4a7b131
rurito <rurito.blog@gmail.com>
2020-07-15 04:26:25 +0900

    Debug用に実装していたボタンなどを削除

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-import firebase from '../../firebase';
+import Header from '../Header';
 
 import Button from '@material-ui/core/Button';
 
-
-
 export default function Main() {
-  const history = useHistory();
-  
-  function Logout() {
-    firebase.auth().signOut();
-    history.push("/");
-  }
-
   return (
     <div>
+      <Header />
       <h1>ここはメインページ</h1>
-      <Button variant="contained" onClick={Logout}>ログアウト</Button>
     </div>
   );
 }
