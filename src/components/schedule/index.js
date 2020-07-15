288a211de77e922e5f22ed8191b3324e199eb9a3
rurito <rurito.blog@gmail.com>
2020-07-15 15:27:02 +0900

    時間割ページに追加

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -8,10 +8,19 @@ import Header from '../Header';
 import Button from '@material-ui/core/Button';
 import { getDefaultNormalizer } from '@testing-library/react';
 
+/**
+ * 時間割ページを表示する関数
+ * @param {Object} props - ユーザーの情報を保持している
+ * @param {string} props.user - Google認証した際に得られるuseridを保持している
+ */
 export default function Schedule(props) {
 
+  /** 既に登録されている時間割list */
   const [scheduleList, setScheduleList] = useState([]);
 
+  /**
+   * 既に登録されている時間割を取得している
+   */
   useEffect(() => {
     const data = async () => {
       await getData();
@@ -19,6 +28,10 @@ export default function Schedule(props) {
     data();
   });
 
+  /**
+   * firestoreに存在している時間割を取得している
+   * 取得した時間割の中で、ログイン中のユーザーが登録した時間割のみ時間割listに追加している
+   */
   async function getData() {
     const colRef = db.collection("schedule");
     const snapshots = await colRef.get();
