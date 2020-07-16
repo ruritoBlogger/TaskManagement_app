a540a0002d8fe9653dc9e73a9447754cd193a472
rurito <rurito.blog@gmail.com>
2020-07-16 18:02:26 +0900

    時間割を削除するボタンを追加

--- src/components/schedule/showSchedule.js
+++ src/components/schedule/showSchedule.js
@@ -1,7 +1,10 @@
 import React from "react"
 
 import LessonDialog from "./LessonDialog"
+import DeleteDialog from "../deleteDialog"
 import firebase, { db } from "../../firebase"
+
+import Grid from "@material-ui/core/Grid"
 
 /**
  * 時間割の内容を表示する関数
@@ -19,18 +22,42 @@ export default function ShowSchedule(props) {
     //setNeedLoad(!needLoad)
   }
 
+  /**
+   * 呼び出されると時間割を削除する
+   */
+  function deleteSchedule() {
+    //setNeedLoad(!needLoad)
+  }
+
   return (
     <div>
-      {
-        (() => {
-          if (props.schedule) {
-            return(<p>{props.schedule.title}</p>)
-          } else {
-            return(<p>読み込めてない</p>)
+      <Grid container>
+        <Grid item>
+          {
+            (() => {
+              if (props.schedule) {
+                return(<p>{props.schedule.title}</p>)
+              } else {
+                return(<p>読み込めてない</p>)
+              }
+            })()
           }
-        })()
-      }
-    <LessonDialog user={props.user} handleSubmit={handleChange} />
+        </Grid>
+        <Grid item>
+          {
+            (() => {
+              if (props.schedule) {
+                return(
+                  <DeleteDialog Button="削除" msg={props.schedule.title} handleSubmit={deleteSchedule} />
+                )
+              } else {
+                return(<p>読み込めてない</p>)
+              }
+            })()
+          }
+        </Grid>
+      </Grid>
+      <LessonDialog user={props.user} handleSubmit={handleChange} />
     </div>
   )
 }
