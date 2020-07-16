858efb8fd3a7f94b80cd6e445a6945525cd19235
rurito <rurito.blog@gmail.com>
2020-07-17 01:02:28 +0900

    試験的に生成したデータを用いて色を登録出来るようにした

--- src/components/schedule/LessonDialog.js
+++ src/components/schedule/LessonDialog.js
@@ -12,6 +12,10 @@ import DialogActions from "@material-ui/core/DialogActions"
 import DialogContent from "@material-ui/core/DialogContent"
 import DialogContentText from "@material-ui/core/DialogContentText"
 import DialogTitle from "@material-ui/core/DialogTitle"
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
+import Select from "@material-ui/core/Select"
+import MenuItem from "@material-ui/core/MenuItem"
 import { useRadioGroup } from "@material-ui/core"
 
 /** CSSを用いたスタイル定義 */
@@ -24,6 +28,9 @@ const styles = makeStyles({
     "text-align": "center",
     "font-size": "15px"
   },
+  Content: {
+    "margin-bottom": "20px",
+  },
   SubmitButton: {
     color: "#FFFFFF",
     background: "#EDC124",
@@ -33,17 +40,29 @@ const styles = makeStyles({
 
 /**
  * 新しく授業を登録するダイアログを表示する関数
- * @param {Object} props - ユーザーの情報や時間割を更新するイベントを管理
+ * @param {Object} props - ユーザーの情報や授業を追加する際に必要な情報・時間割を更新するイベントを管理
  * @param {string} props.user - Google認証した際に得られるuseridを保持している
  * @param {function} props.handleSubmit - 呼び出すと授業listを取得し直す
+ * @param {Object} props.lesson - 授業を追加する際に必要な情報の集合体
+ * @param {list} props.lesson.ColorData - 授業を追加する際に登録する色データのList
+ * @param {list} props.lesson.DateData - 授業を追加する際に登録する日時のList
  */
 export default function LessonDialog(props) {
 
   /** ダイアログが開かれているかどうかの状態 */
   const [open, setOpen] = useState(false)
+
+  /** 選ばれた色を管理 */
+  const [color, setColor] = useState("")
+
   /** react hook formで用意された変数群 */
   const { register, handleSubmit, control, errors } = useForm()
 
+  let ColorData = []
+  let tmp = {}
+  tmp.id = 1
+  tmp.ColorName = "red"
+  ColorData.push(tmp)
   /** CSSを用いたスタイル定義 */
   const classes = styles()
   /**
@@ -60,6 +79,14 @@ export default function LessonDialog(props) {
    */
   function handleClose() {
     setOpen(false)
+  }
+
+  /**
+   * ダイアログを表示している時に色選択部分で状態が変化発火する
+   * 選択された色を状態に登録する
+   */
+  function  handleChange(event) {
+    setColor(event.target.value)
   }
 
   /**
@@ -83,6 +110,7 @@ export default function LessonDialog(props) {
     } else if (value.title) {
       console.log("ユーザーデータが未定義です")
       console.log("再ログインしてください")
+      console.log(color)
       handleClose()
     }
   }
@@ -93,7 +121,7 @@ export default function LessonDialog(props) {
       <Dialog
         open={open}
         fullWidth={true}
-        maxWidth={"sm"}
+        maxWidth={"xs"}
         onClose={handleClose}
         aria-labelledby="common-dialog-title"
         aria-describedby="common-dialog-description"
@@ -101,8 +129,32 @@ export default function LessonDialog(props) {
         <DialogTitle>授業を追加する</DialogTitle>
         <form onSubmit={handleSubmit(Submit)}>
           <DialogContent>
-            <Grid>
-              <TextField name="title" defaultValue="数学Ⅲ" inputRef={register} />
+            <Grid direction="column">
+              <Grid className={classes.Content}>
+                <TextField name="title" label="授業名" defaultValue="数学Ⅲ" inputRef={register} />
+              </Grid>
+              <Grid className={classes.Content}>
+                <TextField name="classroom" label="教室名" defaultValue="C301" inputRef={register} />
+              </Grid>
+              <Grid className={classes.Content}>
+                <FormControl>
+                  <InputLabel id="color-label">色</InputLabel>
+                  <Select
+                    labelId="color-label"
+                    name="color"
+                    value={color}
+                    onChange={handleChange}
+                  >
+                    {
+                      ColorData.map(item => (
+                        <MenuItem value={item.id}>
+                          {item.ColorName}
+                        </MenuItem>
+                      ))
+                    }
+                  </Select>
+                </FormControl>
+              </Grid>
             </Grid>
           </DialogContent>
 
