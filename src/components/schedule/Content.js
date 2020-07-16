89f29a20c5148f83917bfe7dd2bdb54c32c1fb48
rurito <rurito.blog@gmail.com>
2020-07-16 16:40:44 +0900

    他の時間割部分で時間割listを表示する部分を実装

--- src/components/schedule/Content.js
+++ src/components/schedule/Content.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import Grid from "@material-ui/core/Grid"
+import Paper from "@material-ui/core/Paper"
 
 /** CSSを用いたスタイル定義 */
 const styles = makeStyles({
@@ -15,6 +16,15 @@ const styles = makeStyles({
     margin: "30px 0px",
     "padding-right": "150px",
     "font-weight": "400",
+  },
+  OtherScheduleTitle: {
+    "text-align": "center",
+    "font-weight": "300",
+  },
+  OtherScheduleContent: {
+    "background-color": "#CBCBCB",
+    "min-height": "300px",
+    "max-height": "500px",
   },
 })
 
@@ -44,6 +54,7 @@ export default function Content(props) {
   return (
     <Grid container row alignItems="center" justify="center">
       <Grid item xs={5} className={classes.MainSchedule}>
+        <p>test</p>
         <table>
           <tbody>
             {
@@ -57,8 +68,18 @@ export default function Content(props) {
           </tbody>
         </table>
       </Grid>
-      <Grid item xs={3} className={classes.OtherScedules}>
-        <p>他の時間割達</p>
+      <Grid container item direction="column" xs={3}>
+        <Grid item>
+          <h2 className={classes.OtherScheduleTitle}>他の時間割</h2>
+        </Grid>
+        <Grid item direction="column" alignItems="center" justify="center" className={classes.OtherScheduleContent}>
+          {props.scheduleList.map(item => (
+            <Paper elevation={3}>
+              <h3>{item.title}</h3>
+            </Paper>
+          ))}
+          <p>中身</p>
+        </Grid>
       </Grid>
     </Grid>
   )
